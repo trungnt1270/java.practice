1dcd5d4470713bbdf4c0754fe23cc9c179228a9c
trungnt1270 <71129889+trungnt1270@users.noreply.github.com>
2021-03-15 18:18:57 +0700

    practice

---
+++ Flight.java
@@ -0,0 +1,28 @@
+import javax.xml.namespace.QName;
+
+public class Flight {
+    private int number;
+    private String destination;
+
+    public Flight(){
+        number = 0;
+        destination = "";
+    }
+
+    public Flight(int number, String destination){
+        this.number = number;
+        this.destination = destination;
+    }
+
+    public void display(){
+        System.out.println(number + ", " + destination);
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+}
